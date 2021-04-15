990b6f6a0d939ae0219cdca37533c2190eb6ebea
Stefan Tanovic <stefan.tanovic@harness.io>
2021-04-15 15:04:33 +0200

    Android package renaming

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
